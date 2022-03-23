c34c1740a7eddb2bc247acb21747bb3084d6157e
DARKEMPIRESLBOTS <95665347+DARKEMPIRESLBOTS@users.noreply.github.com>
2022-03-23 09:02:36 +0530

    Update converter.py

--- DARK_MUSIC/services/converter/converter.py
+++ DARK_MUSIC/services/converter/converter.py
@@ -1,7 +1,7 @@
 import asyncio
 from os import path
 
-from VCPlayBot.helpers.errors import FFmpegReturnCodeError
+from DARK_MUSIC.helpers.errors import FFmpegReturnCodeError
 
 
 async def convert(file_path: str) -> str:
