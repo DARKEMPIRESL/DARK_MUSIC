352150ccbac585c78846c102a931ad8881c40e31
DARKEMPIRESLBOTS <95665347+DARKEMPIRESLBOTS@users.noreply.github.com>
2022-03-23 09:06:33 +0530

    Update __init__.py

--- DARK_MUSIC/services/queues/__init__.py
+++ DARK_MUSIC/services/queues/__init__.py
@@ -1,7 +1,7 @@
-from VCPlayBot.services.queues.queues import clear 
-from VCPlayBot.services.queues.queues import get
-from VCPlayBot.services.queues.queues import is_empty
-from VCPlayBot.services.queues.queues import put
-from VCPlayBot.services.queues.queues import task_done
+from DARK_MUSIC.services.queues.queues import clear 
+from DARK_MUSIC.services.queues.queues import get
+from DARK_MUSIC.services.queues.queues import is_empty
+from DARK_MUSIC.services.queues.queues import put
+from DARK_MUSIC.services.queues.queues import task_done
 
 __all__ = ["clear", "get", "is_empty", "put", "task_done"]
